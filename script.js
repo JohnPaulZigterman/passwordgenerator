184969ab5a92059be6eff8f472a9020e14dc1456
JohnPaulZigterman <johnpaulzigterman@gmail.com>
2023-08-19 18:49:17 -0400

    commented and streamlined javascript

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
 // Assignment Code
+
+//assigns button variable to #generate ID from HTML file
 var generateBtn = document.querySelector("#generate");
+
+//sets the generatePassword() function
 
 function generatePassword() {
 
+  //Establishes four variables for character libraries, including objects detailing their libraries and whether they will be used
+
   var lowercase = {
     library: "abcdefghijklmnopqrstuvwxyz",
-    use: "N",
+    use: "",
   }
 
   var uppercase = {
     library: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    use: "N",
+    use: "",
   }
   
   var numerals = {
     library: "0123456789",
-    use: "N",
+    use: "",
   }
 
   var special = {
     library: "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
-    use: "N",
+    use: "",
 }
+
+  //establishes a variable into which to concatenate relevant character libraries
 
   var allchars = "";
 
+  //establishes a variable into which to randomly add characters from the concatenated character libraries
+
   var passgen = "";
+
+  //prompts whether to use the four character libraries and concatenates them sequentially based on their .use object value
+  //All four libraries are selected to be used by default
 
   lowercase.use = prompt("Use lowercase characters? Y/N", "Y");
 
@@ -51,8 +64,14 @@ function generatePassword() {
     allchars = allchars.concat(special.library);
   }
 
+  //sets the getpasslength() function
+
   function getpasslength() {
+
+    //prompts for the digits variable to establish password length
     var digits = prompt("Enter Password Length, 8-128", 12);
+
+    //checks that digits variable is a number between 8-128
     if (typeof digits != Number) {
       getpasslength;
     } else if (digits < 8) {
@@ -60,11 +79,16 @@ function generatePassword() {
     } else if (digits > 128) {
       getpasslength();
     }
+
+    //returns the digits variable's numerical value to the getpasslength() function
     return digits;
   }
 
+  //sets passlength variable to the value kicked up by getpasslength() function
   var passlength = getpasslength();
 
+  //checks that passgen variable is shorter than requested password length
+  //if it is shorter, it adds a random value to passgen variable from the concatenated character libraries
   while (passlength > passgen.length) {
     passgen += allchars[Math.floor(Math.random() * allchars.length)];
   }
@@ -73,7 +97,7 @@ function generatePassword() {
 
 }
 
-// Write password to the #password input
+// Writes generatePassword() output data to password variable and then from there to the #password ID
 function writePassword() {
 
   var password = generatePassword();
@@ -83,5 +107,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// Add event listener to generate button, triggers writePassword function when button is clicked
 generateBtn.addEventListener("click", writePassword);
