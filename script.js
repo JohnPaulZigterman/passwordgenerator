59167a8202576afce38db247595a73be09208847
JohnPaulZigterman <johnpaulzigterman@gmail.com>
2023-08-19 12:27:05 -0400

    implemented objects, character library concatenation, and generation logic

--- script.js
+++ script.js
@@ -3,24 +3,61 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
-  var lowercase = "abcdefghijklmnopqrstuvwxyz";
-  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numerals = "0123456789";
-  var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-
-  var uselower = prompt("Use lowercase characters? Y/N", "Y");
-  var useupper = prompt("Use uppercase characters? Y/N", "Y");
-  var usenumerals = prompt("Use numeric characters? Y/N", "Y");
-  var usespecial = prompt("Use special characters? Y/N", "Y");
-  var passlength = prompt("Enter Password Length", "12");
-
-  var allcharacters = "";
-
-  if (uselower == "Y") {
-    var allcharacters = allcharacters.concat(lowercase);
+  var lowercase = {
+    library: "abcdefghijklmnopqrstuvwxyz",
+    use: "N",
   }
 
-  console.log(allcharacters);
+  var uppercase = {
+    library: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    use: "N",
+  }
+  
+  var numerals = {
+    library: "0123456789",
+    use: "N",
+  }
+
+  var special = {
+    library: "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
+    use: "N",
+}
+
+  var allchars = "";
+
+  var passgen = "";
+
+  lowercase.use = prompt("Use lowercase characters? Y/N", "Y");
+
+  if (lowercase.use == "Y") {
+    allchars = allchars.concat(lowercase.library);
+  }
+    
+  uppercase.use = prompt("Use uppercase characters? Y/N", "Y");
+
+  if (uppercase.use == "Y") {
+    allchars = allchars.concat(uppercase.library);
+  }
+  
+  numerals.use = prompt("Use numeric characters? Y/N", "Y");
+
+  if (numerals.use == "Y") {
+    allchars = allchars.concat(numerals.library);
+  }
+  
+  special.use = prompt("Use special characters? Y/N", "Y");
+
+  if (special.use == "Y") {
+    allchars = allchars.concat(special.library);
+  }
+
+  var passlength = prompt("Enter Password Length, 8-128", 12);
+
+  while (passlength > passgen.length) {
+    passgen += allchars[Math.floor(Math.random() * allchars.length)];
+  }
+
+  return passgen;
 
 }
 
